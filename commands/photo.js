ecf1c67ef109f06ad0aa28e218a8ad5906767303
yewon kang <kyeah01@gmail.com>
2021-06-13 23:58:15 +0900

    update img upload

--- commands/photo.js
+++ commands/photo.js
@@ -1,16 +1,16 @@
-const { Command } = require('discord.js-commando');
+// const { Command } = require('discord.js-commando');
 
-module.exports = class MeowCommand extends Command {
-	constructor(client) {
-		super(client, {
-			name: 'meow',
-			group: 'first',
-			memberName: 'meow',
-			description: 'Replies with a meow, kitty cat.',
-		});
-	}
+// module.exports = class MeowCommand extends Command {
+// 	constructor(client) {
+// 		super(client, {
+// 			name: 'meow',
+// 			group: 'first',
+// 			memberName: 'meow',
+// 			description: 'Replies with a meow, kitty cat.',
+// 		});
+// 	}
 
-	run(message) {
-		return message.say('Meow!');
-	}
-}
+// 	run(message) {
+// 		return message.say('Meow!');
+// 	}
+// }
--- hello.js
+++ hello.js
@@ -35,6 +35,9 @@ cuteMessage = (text) => {
     if (text.includes("금동")) {
         return "불렀냥?"
     }
+    if (text.includes("사진")) {
+        return "https://img1.daumcdn.net/thumb/R720x0.q80/?scode=mtistory2&fname=http%3A%2F%2Fcfile7.uf.tistory.com%2Fimage%2F24283C3858F778CA2EFABE"
+    }
     return false
 }
 
